79990bdd3efe75ea497086bb82e7facf70d3b437
Maxim Noverin <noverin@yandex.ru>
2020-04-12 21:11:55 +0500

    demo build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-paddle-game/'
+    : '/',
+  outputDir: 'docs',
+  productionSourceMap: false,
   devServer: {
     overlay: false,
   },
